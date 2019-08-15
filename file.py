da0066ed5890474b418fcb1ed4c13e53dcb869d1
ccppoo <shmishmi79@gmail.com>
2019-08-15 12:56:41 +0900

    Update file.py

--- file.py
+++ file.py
@@ -4,8 +4,8 @@ def check_file(input_dir, output_dir, Release_version = True):
     file_list = []
     output_file_list = []
     files_removed = []
-    input_dir = os.listdir(input_dir_path)
-    output_dir = os.listdir(output_dir_path)
+    input_dir = os.listdir(input_dir)
+    output_dir = os.listdir(output_dir)
 
     for file_in in input_dir:
         if not file_in.startswith('.'):
