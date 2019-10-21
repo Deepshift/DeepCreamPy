40ebc0c9e2a480e121eebd2ea9586a8a2f53a14b
ccppoo <shmishmi79@gmail.com>
2019-10-21 17:07:23 +0900

    signal for updating GUI, progressWindow showing progressbar

---
+++ signals.py
@@ -0,0 +1,18 @@
+from PySide2 import QtCore
+
+# Signals used for sharing status between threads(gui thread <-> decensoring thread)
+class Signals(QtCore.QObject):
+
+    # str : tells status (print in cmd for debug)
+    # int : value to change
+    # usage example in other class(thread) :
+    # → self.signals.total_ProgressBar_update_MAX_VALUE.emit("update value :"+str(max), max)
+    total_ProgressBar_update_MAX_VALUE = QtCore.Signal(str, int)
+    total_ProgressBar_update_VALUE = QtCore.Signal(str, int)
+
+    singal_ProgressBar_update_MAX_VALUE = QtCore.Signal(str, int)
+    singal_ProgressBar_update_VALUE = QtCore.Signal(str, int)
+
+    # str : tells status (print in cmd for debug)
+    # str : String to update label
+    update_progress_LABEL = QtCore.Signal(str, str)
