7303c3d8ec6807cbd827383dbf2e28cede1d50bc
ccppoo <shmishmi79@gmail.com>
2020-01-03 01:51:56 +0900

    add/remove signals that are/aren't used

--- signals.py
+++ signals.py
@@ -1,23 +1,10 @@
 from PySide2 import QtCore
 
-# Signals used for sharing status between threads(gui thread <-> decensoring thread)
+# Signals used for sharing status between threads(IPC, InterProcess Connection)
 class Signals(QtCore.QObject):
 
-    # str : tells status (print in cmd for debug)
-    # int : value to change
     # usage example in other class(thread) :
-    # → self.signals.total_ProgressBar_update_MAX_VALUE.emit("update value :"+str(max), max)
-    total_ProgressBar_update_MAX_VALUE = QtCore.Signal(str, int)
-    total_ProgressBar_update_VALUE = QtCore.Signal(str, int)
-
-    signal_ProgressBar_update_MAX_VALUE = QtCore.Signal(str, int)
-    signal_ProgressBar_update_VALUE = QtCore.Signal(str, int)
-
-    # str : tells status (print in cmd for debug)
-    # str : String to update label
-    update_progress_LABEL = QtCore.Signal(str, str)
-
-    ############ new ############
+    # → self.signals.<method_name>.emit(<parameters - type strict>)
 
     # str : String to update label
     # direct connect to decensorButton.setText(str)
