8ffd1f5c6dc32f1b53539a94729aa4f7d091e89b
Nautics889 <cyberukr@gmail.com>
2024-11-14 21:39:26 +0200

    feat: checkbox for cleaning input dirs (#7)
    * (feat) add a checkbox in `MainWindow` to enable cleaning input directories
      after processing images.
    * (feat) add `clean_up_input_dirs` flag attribute in `Decensor` class to
      manage the cleanup operation.
    * (feat) add `clean_input_directories()` method in `Decensor`
      to remove `.png`, `.jpg`, and `.jpeg` files from input directories.
    * (feat) add `do_post_jobs()` method being called after decensoring process is
      completed.
    * (feat) integrated logic to set `clean_up_input_dirs` via a signal emitted
      from `MainWindow`.

--- config.py
+++ config.py
@@ -24,6 +24,7 @@ def get_args():
 	parser.add_argument('--decensor_input_path', default='./decensor_input/', help='input images with censored regions colored green to be decensored by decensor.py path')
 	parser.add_argument('--decensor_input_original_path', default='./decensor_input_original/', help='input images with no modifications to be decensored by decensor.py path')
 	parser.add_argument('--decensor_output_path', default='./decensor_output/', help='output images generated from running decensor.py path')
+	parser.add_argument('--clean-up-input-dirs', dest='clean_up_input_dirs', action='store_true', default=False, help='whether to delete all image files in the input directories when decensoring is done')
 
 	#Decensor settings
 	parser.add_argument('--mask_color_red', default=0, help='red channel of mask color in decensoring')
